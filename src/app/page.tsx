dc162fa19c4d6112adb1175f8e96efb67e4222d5
Harsha Bhadra <harshahelloworld@gmail.com>
2025-04-13 15:57:43 +0000

    Make the text "Steps to delete your account" bold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-background p-4">
       <p className="text-2xl font-bold mb-1">BillionTests</p>
-      <p className="text-muted-foreground mb-4">Steps to delete your account</p>
+      <p className="text-muted-foreground mb-4"><b>Steps to delete your account</b></p>
       <p className="text-muted-foreground mb-4 text-center">
         To delete you account in BillionTests app please write an email to <a href="mailto:outreach@excl.ai" className="text-primary underline">outreach@excl.ai</a>.
         Your account will be deleted (All data) within 3-5 business days.
